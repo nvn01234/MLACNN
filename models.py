4ce99ce0b2d77ca140b0cfc70758c79fb087fdca
nhatnv <nguyenvannhat152@gmail.com>
2018-03-04 17:23:54 +0900

    base

--- models.py
+++ models.py
@@ -9,15 +9,15 @@ def build_model():
     words_input = Input(shape=[SEQUENCE_LEN, WORD_EMBED_SIZE], dtype='float32')
     pos1_input = Input(shape=[SEQUENCE_LEN, POSITION_EMBED_SIZE], dtype='float32')
     pos2_input = Input(shape=[SEQUENCE_LEN, POSITION_EMBED_SIZE], dtype='float32')
-    e1_input = Input(shape=[3, WORD_EMBED_SIZE], dtype='float32')
-    e2_input = Input(shape=[3, WORD_EMBED_SIZE], dtype='float32')
+    e1_input = Input(shape=[WORD_EMBED_SIZE], dtype='float32')
+    e2_input = Input(shape=[WORD_EMBED_SIZE], dtype='float32')
 
     input_repre = Concatenate()([words_input, pos1_input, pos2_input])
     # input_repre = Dropout(DROPOUT)(input_repre)
 
     # attention
-    # alpha = input_attention(words_input, e1_input,e2_input)
-    # input_repre = Multiply()([input_repre, alpha])
+    alpha = input_attention(words_input, e1_input,e2_input)
+    input_repre = Multiply()([input_repre, alpha])
 
     pooled = conv_maxpool(input_repre)
     e = entities_features(e1_input, e2_input)
@@ -31,7 +31,7 @@ def build_model():
 def input_attention(words_input, e1_input, e2_input):
     e1 = RepeatVector(SEQUENCE_LEN)(e1_input)
     h1 = Concatenate()([words_input, e1])
-    u1 = Dense(1, activation="tanh")(h1)
+    u1 = Dense(WORD_EMBED_SIZE, activation="tanh")(h1)
     alpha1 = Dense(1, activation="softmax")(u1)
     alpha1 = Reshape([SEQUENCE_LEN])(alpha1)
     alpha1 = RepeatVector(WORD_REPRE_SIZE)(alpha1)
@@ -39,7 +39,7 @@ def input_attention(words_input, e1_input, e2_input):
 
     e2 = RepeatVector(SEQUENCE_LEN)(e2_input)
     h2 = Concatenate()([words_input, e2])
-    u2 = Dense(1, activation="tanh")(h2)
+    u2 = Dense(WORD_EMBED_SIZE, activation="tanh")(h2)
     alpha2 = Dense(1, activation="softmax")(u2)
     alpha2 = Reshape([SEQUENCE_LEN])(alpha2)
     alpha2 = RepeatVector(WORD_REPRE_SIZE)(alpha2)
@@ -61,15 +61,13 @@ def conv_maxpool(input_repre):
     return pooled
 
 def entities_features(e1_input, e2_input):
-    e1_flat = Flatten()(e1_input)
-    e2_flat = Flatten()(e2_input)
-    flat = Concatenate()([e1_flat, e2_flat])
-    return flat
+    e = Concatenate()([e1_input, e2_input])
+    return e
 
 def MLP(features):
     output = Concatenate()(features)
     output = Dropout(DROPOUT)(output)
-    output = Dense(HIDDEN_LAYER)(output)
+    output = Dense(OUTPUT_HIDDEN_LAYER)(output)
     output = Dense(units=NB_RELATIONS, activation="softmax")(output)
     return output
 

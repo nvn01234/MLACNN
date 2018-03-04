1c810c0ba2fce4c327d844f3a61989d39662ef96
nhatnv <nguyenvannhat152@gmail.com>
2018-03-04 12:09:45 +0900

    fix

--- models.py
+++ models.py
@@ -8,10 +8,15 @@ def build_model():
     words_input = Input(shape=[SEQUENCE_LEN, WORD_EMBED_SIZE], name="word_input", dtype='float32')
     pos1_input = Input(shape=[SEQUENCE_LEN, POSITION_EMBED_SIZE], name="pos1_input", dtype='float32')
     pos2_input = Input(shape=[SEQUENCE_LEN, POSITION_EMBED_SIZE], name="pos2_input", dtype='float32')
+    e1_input = Input(shape=[WORD_EMBED_SIZE], name="e1_input", dtype='float32')
+    e2_input = Input(shape=[WORD_EMBED_SIZE], name="e2_input", dtype='float32')
+
     input_repre = Concatenate(name="input_representation")([words_input, pos1_input, pos2_input])
     conv = Conv1D(filters=NB_FILTERS, kernel_size=WINDOW_SIZE, padding="same", activation="tanh", name="convolution")(input_repre)
     pool = GlobalMaxPool1D(name="max_pooling")(conv)
-    output = Dropout(name="dropout", rate=DROPOUT)(pool)
+
+    output = Concatenate(name="concat")([pool, e1_input, e2_input])
+    output = Dropout(name="dropout", rate=DROPOUT)(output)
     output = Dense(name="output", units=NB_RELATIONS)(output)
 
     model = Model(inputs=[words_input, pos1_input, pos2_input], outputs=[output])

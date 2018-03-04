26f8a156863a453212e59dfa379d00b92d6b8fcd
nhatnv <nguyenvannhat152@gmail.com>
2018-03-04 15:41:32 +0900

    fix

--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 from settings import *
-from keras.layers import Input, Concatenate, Conv1D, GlobalMaxPool1D, Dense, Dropout, Reshape, Layer, RepeatVector
+from keras.layers import Input, Concatenate, Conv1D, GlobalMaxPool1D, Dense, Dropout, Reshape, Layer, RepeatVector, Permute, Multiply, Average
 from keras.engine import Model
 from keras import backend as K
 from keras import  initializers, regularizers
 
 def build_model():
+    # input
     words_input = Input(shape=[SEQUENCE_LEN, WORD_EMBED_SIZE], dtype='float32')
     pos1_input = Input(shape=[SEQUENCE_LEN, POSITION_EMBED_SIZE], dtype='float32')
     pos2_input = Input(shape=[SEQUENCE_LEN, POSITION_EMBED_SIZE], dtype='float32')
@@ -12,14 +13,36 @@ def build_model():
     e2_input = Input(shape=[WORD_EMBED_SIZE], dtype='float32')
 
     input_repre = Concatenate()([words_input, pos1_input, pos2_input])
-    input_repre = Dropout(DROPOUT)(input_repre)
-    input_att = InputAttention()([input_repre, words_input, e1_input, e2_input])
 
+    # attention
+    e1 = RepeatVector(SEQUENCE_LEN)(e1_input)
+    h1 = Concatenate()([words_input, e1])
+    u1 = Dense(1, activation="tanh")(h1)
+    alpha1 = Dense(1, activation="softmax")(u1)
+    alpha1 = Reshape([SEQUENCE_LEN])(alpha1)
+    alpha1 = RepeatVector(WORD_REPRE_SIZE)(alpha1)
+    alpha1 = Permute([2, 1])(alpha1)
+
+    e2 = RepeatVector(SEQUENCE_LEN)(e2_input)
+    h2 = Concatenate()([words_input, e2])
+    u2 = Dense(1, activation="tanh")(h2)
+    alpha2 = Dense(1, activation="softmax")(u2)
+    alpha2 = Reshape([SEQUENCE_LEN])(alpha2)
+    alpha2 = RepeatVector(WORD_REPRE_SIZE)(alpha2)
+    alpha2 = Permute([2, 1])(alpha2)
+
+    alpha = Average()([alpha1, alpha2])
+    input_repre = Multiply()([input_repre, alpha])
+
+
+    # convolution
     conv = Conv1D(filters=NB_FILTERS,
                   kernel_size=WINDOW_SIZE,
                   padding="same",
-                  activation="tanh")(input_att)
+                  activation="tanh")(input_repre)
     pool = GlobalMaxPool1D()(conv)
+
+    # fully connected
     output = Dropout(DROPOUT)(pool)
     output = Dense(units=NB_RELATIONS, activation="softmax")(output)
 
@@ -28,33 +51,6 @@ def build_model():
     model.summary()
     return model
 
-
-class InputAttention(Layer):
-    def compute_output_shape(self, input_shape):
-        return [input_shape[0], SEQUENCE_LEN, WORD_REPRE_SIZE]
-
-    def build(self, input_shape):
-        self.W = self.add_weight([WORD_EMBED_SIZE, WORD_EMBED_SIZE],
-                                 initializer=initializers.get('truncated_normal'),
-                                 regularizer=regularizers.get(None),
-                                 name="W")
-        self.built = True
-
-    def call(self, inputs, **kwargs):
-        input_repre, words_input, e1, e2 = inputs
-
-        A1 = K.dot(words_input, self.W)  # (?, SEQUENCE_LEN, WORD_EMBED_SIZE)
-        A1 = K.batch_dot(A1, e1, (2, 1))  # (?, SEQUENCE_LEN)
-        alpha1 = K.softmax(A1)
-
-        A2 = K.dot(words_input, self.W)
-        A2 = K.batch_dot(A2, e2, (2, 1))
-        alpha2 = K.softmax(A2)
-
-        alpha = (alpha1 + alpha2)/2
-        alpha = K.expand_dims(alpha)
-        output = input_repre * alpha
-        return output
 
 
 if __name__ == "__main__":

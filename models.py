a4b3f90e78dc2d9ed0e5ccd4dbd8c59c003ab28e
nhatnv <nguyenvannhat152@gmail.com>
2018-03-04 13:30:00 +0900

    fix

--- models.py
+++ models.py
@@ -1,29 +1,30 @@
 from settings import *
-from keras.layers import Input, Concatenate, Conv1D, GlobalMaxPool1D, Dense, Dropout
+from keras.layers import Input, Concatenate, Conv1D, GlobalMaxPool1D, Dense, Dropout, Reshape
 from keras.engine import Model
 from keras.optimizers import Adam
 
 
 def build_model():
-    words_input = Input(shape=[SEQUENCE_LEN, WORD_EMBED_SIZE], name="word_input", dtype='float32')
-    pos1_input = Input(shape=[SEQUENCE_LEN, POSITION_EMBED_SIZE], name="pos1_input", dtype='float32')
-    pos2_input = Input(shape=[SEQUENCE_LEN, POSITION_EMBED_SIZE], name="pos2_input", dtype='float32')
-    e1_input = Input(shape=[WORD_EMBED_SIZE], name="e1_input", dtype='float32')
-    e2_input = Input(shape=[WORD_EMBED_SIZE], name="e2_input", dtype='float32')
+    words_input = Input(shape=[SEQUENCE_LEN, WORD_EMBED_SIZE], dtype='float32')
+    pos1_input = Input(shape=[SEQUENCE_LEN, POSITION_EMBED_SIZE], dtype='float32')
+    pos2_input = Input(shape=[SEQUENCE_LEN, POSITION_EMBED_SIZE], dtype='float32')
+    e1_input = Input(shape=[WORD_EMBED_SIZE], dtype='float32')
+    e2_input = Input(shape=[WORD_EMBED_SIZE], dtype='float32')
 
-    input_repre = Concatenate(name="input_representation")([words_input, pos1_input, pos2_input])
+    input_repre = Concatenate()([words_input, pos1_input, pos2_input])
+    input_repre = Dropout(rate=DROPOUT)(input_repre)
 
     pooled_output = []
-    for window_size in [3, 4, 5]:
+    for window_size in WINDOW_SIZES:
         conv = Conv1D(filters=NB_FILTERS, kernel_size=window_size, padding="same", activation="tanh")(input_repre)
         pool = GlobalMaxPool1D()(conv)
         pooled_output.append(pool)
-    conv = Concatenate(name="convolution_concat")(pooled_output)
-    output = Concatenate(name="concat")([conv, e1_input, e2_input])
-    output = Dropout(name="dropout", rate=DROPOUT)(output)
-    output = Dense(name="output", units=NB_RELATIONS)(output)
+    output = Concatenate(name="concat")([*pooled_output, e1_input, e2_input])
+    output = Dropout(rate=DROPOUT)(output)
+    output = Dense(units=NB_RELATIONS)(output)
 
     model = Model(inputs=[words_input, pos1_input, pos2_input, e1_input, e2_input], outputs=[output])
+    optimizer = Adam(lr=LEARNING_RATE)
     model.compile(loss="sparse_categorical_crossentropy", metrics=["accuracy"], optimizer='adam')
     model.summary()
     return model

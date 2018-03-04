0c70a9e5b1f3f5a95de3017f009c21f67789a5e8
nhatnv <nguyenvannhat152@gmail.com>
2018-03-04 14:29:22 +0900

    fix

--- models.py
+++ models.py
@@ -13,13 +13,9 @@ def build_model():
     input_repre = Concatenate()([words_input, pos1_input, pos2_input])
     input_repre = Dropout(rate=DROPOUT)(input_repre)
 
-    pooled_output = []
-    for window_size in WINDOW_SIZES:
-        conv = Conv1D(filters=NB_FILTERS, kernel_size=window_size, padding="same", activation="tanh")(input_repre)
-        pool = GlobalMaxPool1D()(conv)
-        pooled_output.append(pool)
-    output = Concatenate(name="concat")([*pooled_output, e1_input, e2_input])
-    output = Dropout(rate=DROPOUT)(output)
+    conv = Conv1D(filters=NB_FILTERS, kernel_size=WINDOW_SIZE, padding="same", activation="tanh")(input_repre)
+    pool = GlobalMaxPool1D()(conv)
+    output = Dropout(rate=DROPOUT)(pool)
     output = Dense(units=NB_RELATIONS, activation="softmax")(output)
 
     model = Model(inputs=[words_input, pos1_input, pos2_input, e1_input, e2_input], outputs=[output])

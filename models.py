3b4a7826a344e1bf6b9ee1af795b23ad78545040
nhatnv <nguyenvannhat152@gmail.com>
2018-03-04 13:17:34 +0900

    fix

--- models.py
+++ models.py
@@ -12,14 +12,18 @@ def build_model():
     e2_input = Input(shape=[WORD_EMBED_SIZE], name="e2_input", dtype='float32')
 
     input_repre = Concatenate(name="input_representation")([words_input, pos1_input, pos2_input])
-    conv = Conv1D(filters=NB_FILTERS, kernel_size=WINDOW_SIZE, padding="same", activation="tanh", name="convolution")(input_repre)
-    pool = GlobalMaxPool1D(name="max_pooling")(conv)
 
-    output = Concatenate(name="concat")([pool, e1_input, e2_input])
+    pooled_output = []
+    for window_size in [3, 4, 5]:
+        conv = Conv1D(filters=NB_FILTERS, kernel_size=window_size, padding="same", activation="tanh")(input_repre)
+        pool = GlobalMaxPool1D(name="max_pooling")(conv)
+        pooled_output.append(pool)
+    conv = Concatenate(name="convolution_concat")(pooled_output)
+    output = Concatenate(name="concat")([conv, e1_input, e2_input])
     output = Dropout(name="dropout", rate=DROPOUT)(output)
     output = Dense(name="output", units=NB_RELATIONS)(output)
 
     model = Model(inputs=[words_input, pos1_input, pos2_input, e1_input, e2_input], outputs=[output])
-    model.compile(loss="sparse_categorical_crossentropy", metrics=["accuracy"], optimizer='adadelta')
+    model.compile(loss="sparse_categorical_crossentropy", metrics=["accuracy"], optimizer='sgd')
     model.summary()
     return model

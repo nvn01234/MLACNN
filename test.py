ddb3318aeeda46f6e004874692482ed128e00e84
nhatnv <nguyenvannhat152@gmail.com>
2018-03-06 03:15:58 +0900

    fix

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ def main():
     y_pred = np.load("log/y_pred_3.npy")
     lines = []
     for idx, y in zip(test_idx, y_pred):
-        line = "%d\t%s" % (idx, idx2relations[y])
+        line = "%s\t%s" % (idx, idx2relations[y])
         lines.append(line)
     with open("log/answer_keys.txt", "w", encoding="utf8") as f:
         f.write("\n".join(lines))

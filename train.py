5e4d3fb195e458b172d66421cac9d2ae04342125
nhatnv <nguyenvannhat152@gmail.com>
2018-03-06 17:36:40 +0900

    fix

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ def main():
     print("testing")
     scores = model.predict(x_test, verbose=False)
     predictions = scores.argmax(-1)
-    gen_answer_key(predictions, name="chars_200")
+    gen_answer_key(predictions, name="chars_5")
 
 
 if __name__ == "__main__":

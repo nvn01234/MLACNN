2f5329262dbbe7d8bff1130954704a2476ca940c
nhatnv <nguyenvannhat152@gmail.com>
2018-03-21 23:09:20 +0700

    update

--- train.py
+++ train.py
@@ -11,11 +11,11 @@ from utils import make_dict
 
 def main():
     print("load train data")
-    x_train = [np.load("data/train/%s.npy" % name) for name in ["words", "pos1", "pos2", "e1", "e2", "e1context", "e2context", "tags"]]
+    x_train = [np.load("data/train/%s.npy" % name) for name in ["words", "pos1", "pos2", "e1", "e2", "e1context", "e2context", "chars"]]
     y_train = np.load("data/train/y.npy")
 
     print("load test data")
-    x_test = [np.load("data/test/%s.npy" % name) for name in ["words", "pos1", "pos2", "e1", "e2", "e1context", "e2context", "tags"]]
+    x_test = [np.load("data/test/%s.npy" % name) for name in ["words", "pos1", "pos2", "e1", "e2", "e1context", "e2context", "chars"]]
     y_test = np.load("data/test/y.npy")
 
     print("load embeddings")
@@ -45,8 +45,8 @@ def main():
     scores = model.predict(x_test, verbose=False)
     predictions = scores.argmax(-1)
     meta = """
-POS tagging: yes
-characted-level: no
+POS tagging: No
+characted-level: yes
 attention input: no
 PCNN: no
 """
